72163ec7d03c80f23bc002d4ab5b76e360e1c132
Bijayash Chitrakar <36729904+HECATE1997@users.noreply.github.com>
2025-07-02 19:41:55 +0545

    models and DbContext created

---
+++ LeoShop.API/LeoShop.API/Data/LeoShopCosmosDbContext.cs
@@ -0,0 +1,30 @@
+﻿using LeoShop.OrderManagement.Models;
+using LeoShop.PaymentGateway.Models;
+using LeoShop.ProductCatalog.Models;
+using LeoShop.UserManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeoShop.API.Data
+{
+    public class LeoShopCosmosDbContext : DbContext
+    {
+        public LeoShopCosmosDbContext(DbContextOptions<LeoShopCosmosDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseCosmos(
+                "https://your-cosmos-db-uri.com",
+                "YourCosmosDbKey==",
+                "YourDatabaseName");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+    }
+
+}
---
+++ LeoShop.API/LeoShop.API/Data/LeoShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LeoShop.OrderManagement.Models;
+using LeoShop.PaymentGateway.Models;
+using LeoShop.ProductCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+using LeoShop.UserManagement.Models;
+
+namespace LeoShop.API.Data
+{
+    public class LeoShopDbContext : DbContext
+    {
+        public LeoShopDbContext(DbContextOptions<LeoShopDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+    }
+}
